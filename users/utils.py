8bc77c9ff87521cf664e10155aa22c10c700b827
Geon gyu Park <geongyupark@GeonuiMBookAir2.kornet>
2022-12-09 18:51:08 +0900

    refactor: Fix Error handling Message

--- users/utils.py
+++ users/utils.py
@@ -2,6 +2,8 @@ import jwt
 
 from django.http import JsonResponse
 
+from rest_framework import status
+
 from config.settings import SECRET_KEY, ALGORITHM
 
 from .models import User
@@ -10,23 +12,29 @@ def login_decorator(func):
     def wrapper(self, request, *args, **kwargs):
         try:
             if 'Authorization' not in request.headers:
-                return JsonResponse({"detail" : "No Authorization In Header"}, status = 401)
+                return JsonResponse({"detail" : "Unauthorized User"}, status=status.HTTP_401_UNAUTHORIZED)
             
             access_token = request.headers.get('Authorization')           
             payload      = jwt.decode(access_token, SECRET_KEY, ALGORITHM)
-            user_id      = payload['user_id']
+            user_id      = payload.get('user_id')
+
+            if not user_id:
+                raise jwt.exceptions.DecodeError
 
             request.user = User.objects.get(id = user_id)
 
             return func(self, request, *args, **kwargs)
         
         except jwt.exceptions.DecodeError:
-            return JsonResponse({"detail" : "Invalid Token"}, status = 400)
+            return JsonResponse({"message" : "Invalid Token"}, status=status.HTTP_400_BAD_REQUEST)
         
         except jwt.exceptions.ExpiredSignatureError:
-            return JsonResponse({"detail" : "Expired Token"}, status = 401)
+            return JsonResponse({"message" : "Invalid Token"}, status=status.HTTP_400_BAD_REQUEST)
         
         except User.DoesNotExist:
-            return JsonResponse({"detail" : "Invalid User"}, status = 400)
+            return JsonResponse({"message" : "Invalid User"}, status=status.HTTP_400_BAD_REQUEST)
+
+        except KeyError:
+            return JsonResponse({"message" : "Invalid User"}, status=status.HTTP_400_BAD_REQUEST)
 
     return wrapper
